138e98a01a92503c46d9981048f3a6f8048b36ab
Maymunah Hicks <hicksmd@uab.edu>
2024-08-19 12:10:34 -0500

    Adding Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 
 function App() {
   return (
+    <div className="App">
     <Header />
+    <h1>Welcome to my website</h1>
+    <Footer />
+    </div>
   );
 }
 
+
 export default App;
+
