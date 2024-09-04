f212f25322d93438266a850914c33b6799cb5de5
Maymunah Hicks <hicksmd@uab.edu>
2024-09-04 00:12:38 -0500

    Add text content into home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <div>Home page</div>
-  )
-}
+    <div className="container">
+      <div className="intro">
+        Hello, I am
+        </div>
+        <div className="name-intro">
+          Maymunah Hicks
+        </div>
+        <br />
+        <div className="additional-info">
+        I am an aspiring software engineer who is passionate about learning and
+        taking on new challenges.<br /> Feel free to explore my projects. Thanks for
+        stopping by!
+        </div>
+    </div>
+  );
+};
