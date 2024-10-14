e426ffd0eb4668f4710b7817edeab36a7b37011b
Maymunah Hicks <hicksmd@uab.edu>
2024-10-13 20:07:12 -0500

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,5 +2,35 @@ import React from "react";
 import "./Footer.css";
 
 export const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="contact-info">
+          <p>Maymunah Hicks</p>
+          <p>(704) 685-5241</p>
+        </div>
+        <div className="socials">
+          <p>Follow me:</p>
+          <div className="social-icons">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <img src="/path/to/facebook-icon.png" alt="Facebook" />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <img src="/path/to/twitter-icon.png" alt="Twitter" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <img src="/path/to/instagram-icon.png" alt="Instagram" />
+            </a>
+          </div>
+        </div>
+        <div className="subscribe">
+          <p>Subscribe to my blog</p>
+          <form>
+            <input type="email" placeholder="Enter your email" />
+            <button type="submit">Subscribe</button>
+          </form>
+        </div>
+      </div>
+    </footer>
+  );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import myPic from './my_pic.jpg';
+import { Link } from "react-router-dom";
+import Typed from 'typed.js'; // Import Typed.js
 
 export const Home = () => {
+  useEffect(() => {
+    // Options for Typed.js
+    const options = {
+      strings: [
+        "Hello,", 
+        "My name is Maymunah Hicks.",
+        "I am an aspiring software engineer who is passionate about learning and taking on new challenges.",
+        "Click the button below to learn more about me. Thanks for stopping by!"
+      ],
+      typeSpeed: 45,
+      backSpeed: 20,
+      loop: false,
+      startDelay: 500, // Small delay before typing starts
+      backDelay: 1000, // Delay between strings
+      showCursor: true, // One blinking cursor for the whole typing
+      cursorChar: '|', // Customize the cursor character if you want
+    };
+
+    // Initialize Typed.js with one instance for all text
+    const typed = new Typed('#typed-text', options);
+
+    // Cleanup function to destroy the Typed instance
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="intro-wrapper">
       <div className="intro">
-        <p>Hello, I am</p>
-        <div className="name-intro"><p>Maymunah Hicks</p></div>
-        <div className="additional-info">
-          <p>I am an aspiring software engineer who is passionate about learning
-          and taking on new challenges.
-          <br /> Feel free to explore my projects. Thanks for stopping by!</p>
+        <h2>
+          <span id="typed-text"></span> {/* One element for all text */}
+        </h2>
         </div>
-        <button className="contact-button">Contact Me</button>
+        <div className="about-me">
+        <Link to="/About">
+          <button className="about-me-button">More about me</button>
+        </Link>
       </div>
       <div className="photo">
-      <img src={myPic} alt="Maymunah Hicks" className="photo-style"/>
+        <img src={myPic} alt="Maymunah Hicks" className="photo-style" />
       </div>
     </div>
   );
