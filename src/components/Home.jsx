1e8ac18a845c9239ab9bec5bcd14a028b7387db4
Derrick Strong <derrickmstrong@gmail.com>
2024-10-16 16:44:05 -0500

    Update media queries in small devices; Update typed id and code;

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
 import "./Home.css";
-import myPic from './my_pic.jpg';
+import myPic from "./my_pic.jpg";
 import { Link } from "react-router-dom";
-import Typed from 'typed.js'; // Import Typed.js
+import Typed from "typed.js"; // Import Typed.js
 
 export const Home = () => {
   useEffect(() => {
     // Options for Typed.js
     const options = {
       strings: [
-        "Hello,", 
+        "Hello,",
         "My name is Maymunah Hicks.",
         "I am an aspiring software engineer who is passionate about learning and taking on new challenges.",
-        "Click the button below to learn more about me. Thanks for stopping by!"
+        "Click the button below to learn more about me. Thanks for stopping by!",
       ],
       typeSpeed: 45,
       backSpeed: 20,
@@ -20,11 +20,11 @@ export const Home = () => {
       startDelay: 500, // Small delay before typing starts
       backDelay: 1000, // Delay between strings
       showCursor: true, // One blinking cursor for the whole typing
-      cursorChar: '|', // Customize the cursor character if you want
+      cursorChar: "|", // Customize the cursor character if you want
     };
 
     // Initialize Typed.js with one instance for all text
-    const typed = new Typed('#typed-text', options);
+    const typed = new Typed("#typed-text", options);
 
     // Cleanup function to destroy the Typed instance
     return () => {
@@ -35,11 +35,11 @@ export const Home = () => {
   return (
     <div className="intro-wrapper">
       <div className="intro">
-        <h2>
-          <span id="typed-text"></span> {/* One element for all text */}
+        <h2 id="typed-text">
+          {/* Text will be typed here */}
         </h2>
-        </div>
-        <div className="about-me">
+      </div>
+      <div className="about-me">
         <Link to="/About">
           <button className="about-me-button">More about me</button>
         </Link>
